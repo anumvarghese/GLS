fd9904f77ccd4cae763f5c9fb59def547c1bb858
Anu Mathew <anumathew@Anus-MacBook-Air.local>
2019-05-30 21:42:46 +0530

    Added home page

--- oracle_learing_system/app/views.py
+++ oracle_learing_system/app/views.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+
+import datetime
+import json
+
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def home(request):
+    context = {} 
+    return render(request, 'index.html', context)
+
